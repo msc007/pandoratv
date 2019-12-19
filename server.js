f553cb6e2bf98f1797eb15d35bac250134354e2c
msc007 <alstjr5569@gmail.com>
2019-12-19 14:09:41 -0800

    Change S3 address for CORS

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const ENV = process.env.NODE_ENV || 'dev';
 if( ENV === 'production') {
   console.log('Production Environment');
   const cors = require('cors');
-  app.use(cors({origin: 'http://pandoratv.s3-website-us-west-1.amazonaws.com'}));
+  app.use(cors({origin: 'http://pandoratv.s3-website.us-east-2.amazonaws.com'}));
 }
 
 function createData(name, link, description, language, hits) {
